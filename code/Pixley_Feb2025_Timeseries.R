f963622707499c9e4bb11ead1a0ce9be6ba2b96f
rickard11 <100870004+rickard11@users.noreply.github.com>
2025-02-14 09:52:13 -0800

    Edited code to show daily fluctuation and not daily average.

--- code/Pixley_Feb2025_Timeseries.R
+++ code/Pixley_Feb2025_Timeseries.R
@@ -4,45 +4,46 @@ library(grid)
 library(gridExtra)
 library(dplyr)
 library(tidyr)
-#read in E5 water level
-setwd("//babylon/visitors/rickard/Well analysis/Ranchbot")
-PixleyWell<-read.csv("Pixley_ Tulare_ Capinero creek Well 20250101-20250213.csv")
+#read in water level
+#setwd("//babylon/visitors/rickard/Well analysis/Ranchbot")
+#setwd("C:/Users/ricka/OneDrive/Documents/JLDP-report-analysis")
+PixleyWell<-read.csv("data/raw/Pixley/Pixley_ Tulare_ Capinero creek Well 20241201-20250214.csv")
 head(PixleyWell)
 #Change to date format
 PixleyWell$Date<-format(PixleyWell$Date.and.Time,format="%Y-%m-%d")
 PixleyWell$Date<-as.Date(PixleyWell$Date)
+PixleyWell$Date.and.Time<-as.POSIXct(PixleyWell$Date.and.Time,format="%Y-%m-%d %H:%M:%S")
+PixleyWell<-PixleyWell[PixleyWell$Date.and.Time>="2024-12-12",]
 DailyPixleyWell<-aggregate(ft..below.ground.~Date,PixleyWell, FUN=mean)
-DailyPixleyWell<-DailyPixleyWell[DailyPixleyWell$Date!="2023-10-19",] #Remove first date because measured during deployment
-head(DailyPixleyWell)
 #read in E5 rainfall
-PixleyRain<-read.csv("Pixley_Tulare Rain Gauge Rain Gauge 20250101-20250213.csv")
+PixleyRain<-read.csv("data/raw/Pixley/Pixley_Tulare Rain Gauge Rain Gauge 20241201-20250214.csv")
 head(PixleyRain)
 
 #change to date format
 PixleyRain$Date<-as.Date(PixleyRain$Date,format="%Y-%m-%d (%a)")
 #merge
-E5all<-merge(PixleyRain,DailyPixleyWell,by="Date")
-E5all
+Pixleyall<-merge(PixleyRain,DailyPixleyWell,by="Date")
+
 #Plot
 scaleFUN <- function(x) sprintf("%.1f", x)
-g1 <- ggplot(E5all, aes(x=Date)) +
+g1 <- ggplot(PixleyRain, aes(x=Date)) +
   geom_bar(aes(y=Rain..in.), stat="identity", size=.1, fill="#69b3a2", color="black",alpha=0.4) +
-  ggtitle("Rainfall and Groundwater Depth at Pixley Well (2025 WY to date)")+theme_bw()+ 
+  ggtitle("Rainfall and Groundwater Depth at Pixley Well (2025 WY)")+theme_bw()+ 
   scale_y_reverse(labels=scaleFUN)+ylab("Rainfall (Inches)")+
-  theme(title =element_text(size=15, face='bold'),
-        axis.title.y = element_text(size=14),
-        axis.text.y = element_text(size=13),
-        axis.title.x    = element_blank(),
+  theme(title =element_text(size=14, face='bold'),
         axis.text.x     = element_blank(),
+        axis.title.y = element_text(size=14),
+        axis.text.y = element_text(size=14),
+        axis.title.x    = element_blank(),
         axis.ticks.x    = element_blank())
 
-g2 <- ggplot(E5all, aes(x=Date)) +
-  geom_line( aes(y=ft..below.ground.), size=1.5) +
+g2 <- ggplot(PixleyWell, aes(x=Date.and.Time)) +
+  geom_line( aes(y=ft..below.ground.), size=1) +
   ylab("Groundwater Depth")+
   theme_bw()+ scale_y_reverse()+
-  theme(axis.title.y = element_text(size=14),
+  theme(axis.title.y = element_text(size=14, face='bold'),
         legend.title    = element_blank(),
-        axis.text.y = element_text(size=15),
+        axis.text.y = element_text(size=13),
         axis.text.x = element_text(size=15),
         axis.title.x=element_text(size=14))
 g1 <- ggplot_gtable(ggplot_build(g1))
